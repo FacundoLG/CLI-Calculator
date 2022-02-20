c6b682d67a1fca4a47959fafeea78e184480e8ca
Facundo Gimenez <facu-judoca@hotmail.com.ar>
2022-02-20 11:10:02 -0300

    basic calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env::{args,Args};
+
 fn main() {
+    let mut args:Args = args();
+    println!("{:?}",&args);
+    let first_arg:String = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
+    let second_arg:String = args.nth(0).unwrap();
     
+    let first_num:f32 = first_arg.parse::<f32>().unwrap();
+    let second_num:f32 = second_arg.parse::<f32>().unwrap();
+   
+    let result = resolve_operation(operator, first_num, second_num);
+    println!("{}",result)    
+}
+
+fn resolve_operation(operator:char,first_number:f32, second_number:f32 ) -> f32 {
+    match &operator {
+        '+' => first_number + second_number,
+        '-' => first_number - second_number,
+        '*' | 'x' => first_number * second_number,
+        '/' => first_number / second_number,
+        _ => panic!("Invalido Operation")
+    }
+
 }
